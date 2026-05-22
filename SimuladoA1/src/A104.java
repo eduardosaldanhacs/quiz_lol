8823129a5e860a02cbaa4047c227bf08f1837917
Eduardo <eduardosaldanhacs@gmail.com>
2026-05-22 09:11:24 -0300

    seccond commit

--- SimuladoA1/src/A104.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-//Escreva um algoritmo que leia uma matriz quadrada de sétima ordem M(7,7), 7 linhas
-//e 7 colunas, e efetue a soma dos elementos da diagonal principal (canto superior esquerdo ao canto
-//inferior direito). Depois de calculado mostre a soma na tela.
-public class A104 {
-    public static void main(String[] args) {
-
-    }
-}
